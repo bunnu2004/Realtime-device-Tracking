4553e219a008302910a0dc8c2c47bccd1b1286bb
bunnu2004 <135821213+bunnu2004@users.noreply.github.com>
2024-11-03 15:35:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const path = require("path");
+
+
+// setUp the socket.io
+const http = require("http");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+// SetUp ejs
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname,"public")));
+
+io.on("connection", function (socket) {
+
+    socket.on("send-location", function (data){
+        io.emit("receive-location",{id: socket.id, ...data});
+    });
+
+    // when location disconected
+    socket.on("disconnect", function(){
+        io.emit("user-disconnected", socket.id)
+    });
+
+    console.log("conneted");
+
+});
+
+app.get("/", function (req, res){
+    res.render("index")
+});
+
+
+server.listen(3000);
